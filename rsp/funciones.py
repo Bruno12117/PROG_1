519ec456061d4a4554c1fb7b005131e906a7a499
Bruno12117 <goitiabruno09@gmail.com>
2026-07-15 19:05:50 -0300

    Update funciones.py

--- rsp/funciones.py
+++ rsp/funciones.py
@@ -1,9 +1,11 @@
-MAX_ALUMNOS= 27
 #VALIDACIONES CLAVES DICCIONARIOS 
 def validar_entero(cadena: str) -> bool:
-    """Funcion que valida si una cadena contiene solo dígitos."""
+    """Funcion que valida si una cadena contiene solo dígitos usando una lista de numeros validos para controlar.
+    Args: cadena(str): cadena que se ingresa y se analiza
+    Returns: (bool): TRUE si toda la cadena contiene los numeros que estan en la lista
+                     FALSE si la cadena no contiene ningun numero de los que estan en la lista"""
 
-    valor = True #bandera para no escribir mas de un retorono
+    valor = True  
     if len(cadena) == 0:
         valor = False
     else:
@@ -104,108 +106,219 @@ def validar_nota(mensaje: str):
             print("Error: Debe inresar unicamente numeros enteros")
 
 
-#OPCION 2: Carga de datos
-def cargar_datos(legajos, ape_nombres, generos, parciales1, parciales2):
-    print(f"\nIniciando carga manual de {MAX_ALUMNOS} estudiantes")
-    for i in range(MAX_ALUMNOS):
-        #cuenta del 0 al 26
-        print(f"Carga del estudiante {i + 1}:")
-        legajos.append(validar_legajo())
-        ape_nombres.append(validar_ape_nom())
-        generos.append(validar_genero())
-        parciales1.append(validar_nota("Nota del Primer Parcial (1-10): "))
-        parciales2.append(validar_nota("Nota del Segundo Parcial (1-10): "))
-    print("Carga de datos finalizada con éxito.")
+# ==========================================
+# FUNCIONES DEL MENÚ
+# ==========================================
 
+# Opcion 1: Leer archivo JSON
+def leer_archivo_json():
+    """Funcion que se encarga de leer el archivo JSON, completar la lista
+    hasta 10 elementos y mostrar los datos por consola.
+    Args:  None
+    Return: Retorna el archvio json por consola mediante la variable 'datos' """
+    
+    import json
+    with open('prueba/data_sp.json', 'r') as archivo_json:
+        datos = json.load(archivo_json)
 
-#OPCION 3: Mostrar un elemento
-def mostrar_un_elemento(lista_estudiantes, legajo, nombre, genero, pp, sp, estudiante, promedio=None): # esta mal #lista_estudiantes,legajo, ape_nom, genero, pp, sp, promedio=None
+    for i in range(len(datos)):
+        print(f"ESTUDIANTE {i + 1}") 
+        print(f"legajo: {datos[i]['legajo']}")
+        print(f"ape_nom: {datos[i]['ape_nom']}")
+        print(f"género: {datos[i]['genero']}")
+        print(f"pp: {datos[i]['pp']}")
+        print(f"sp: {datos[i]['sp']}")
+        print(f"prom: {datos[i]['prom']}")
+        print()
+
+    
+    resto_de_estudiantes = 10 - len(datos) 
+    for r in range(resto_de_estudiantes):
+        datos.append({
+            "legajo": 0, "ape_nom": "", "genero": "", "pp": 0, "sp": 0, "prom": 0.0
+        })
+    
+    return datos
+    
+# #OPCION 2: Carga de datos
+def cargar_datos(lista_estudiantes):
+    """Funcion que sirve para cargar los datos de los estudiantes. Si los datos pasan por la validacion correspondiente
+    entonces quedan guardados en las respectivas claves del diccionario
+    Args: lista_estudiantes""" 
+
+    cargado = False
+    for i in range(len(lista_estudiantes)): #10
+        if lista_estudiantes[i]["legajo"] == 0:
+            print(f"Cargando datos del estudiante {i + 1}:")
+            
+            legajo = validar_legajo()
+            nombre = validar_ape_nom()
+            genero = validar_genero()
+            p1 = validar_nota("Nota del Primer Parcial (1-10): ")
+            p2 = validar_nota("Nota del Segundo Parcial (1-10): ")
+
+            lista_estudiantes[i]["legajo"] = legajo
+            lista_estudiantes[i]["ape_nom"] = nombre
+            lista_estudiantes[i]["genero"] = genero
+            lista_estudiantes[i]["pp"] = p1
+            lista_estudiantes[i]["sp"] = p2
+            lista_estudiantes[i]["prom"] = 0.0
+            print("Estudiante cargado con éxito.")
+            cargado = True
+            break    
+        
+    if cargado == False:
+        print("La lista de alumnos esta completa. No hay espacio libre.")
+
+#Opcion 3a: Mostrar un elemento
+def mostrar_un_elemento(estudiante):
+    """Funcion que se encarga de mostrar los datos de cada estudiante usando su correspondiente diccionario.
+    Args: estudiante"""
+
+    promedio = estudiante["prom"]
     if promedio is not None and promedio != 0.0:
-        print("----------------------------------------------------------------")
-        print("| LEGAJO | NOMBRE      | GENERO | Nota PP | Nota SP | PROMEDIO |")
-        print("|--------|-------------|--------|---------|---------|----------|")
-        for estudiante in lista_estudiantes:
-            print(f'|{estudiante["legajo"]:<7} | {estudiante["ape_nom"]:<11} | {estudiante["genero"]:<6} | {estudiante["pp"]:<7} | {estudiante["sp"]:<7} | {estudiante["prom"]:<9}|')
+        print(f'|{estudiante["legajo"]:<7} | {estudiante["ape_nom"]:<18} | {estudiante["genero"]:<6} | {estudiante["pp"]:<7} | {estudiante["sp"]:<7} | {estudiante["prom"]:<8} |')
     else:
-        print("----------------------------------------------------------------")
-        print("| LEGAJO | NOMBRE      | GENERO | Nota PP | Nota SP | PROMEDIO |")
-        print("|--------|-------------|--------|---------|---------|----------|")
-        for estudiante in lista_estudiantes:
-            print(f'|{estudiante["legajo"]:<7} | {estudiante["ape_nom"]:<11} | {estudiante["genero"]:<6} | {estudiante["pp"]:<7} | {estudiante["sp"]:<7}')   
+        print(f'|{estudiante["legajo"]:<7} | {estudiante["ape_nom"]:<18} | {estudiante["genero"]:<6} | {estudiante["pp"]:<7} | {estudiante["sp"]:<7} | {"-":<8} |') 
 
+#Opcion 3b: Recorrer y mostrar
+def recorrer_y_mostrar(lista_estudiantes):
+    """Funcion que imprime la estructura de la tabla y recorre los estudiantes cargados.
+    Args: lista_estudiantes """
 
-#3b: Recorrer
-def recorrer_y_mostrar(legajos, nombres, generos, parciales1, parciales2, promedios_lista): #esta mal
-    print("REPORTE GENERAL DE ESTUDIANTES")
-    for i in range(len(legajos)):
-        if len(promedios_lista) == 0:
-            break #promedios_lista = none
-        else:    
-            mostrar_un_elemento(legajos[i], nombres[i], generos[i], parciales1[i], parciales2[i], promedios_lista[i])
+    print("-----------------------------------------------------------------------")
+    print("| LEGAJO | NOMBRE             | GENERO | Nota PP | Nota SP | PROMEDIO |")
+    print("|--------|--------------------|--------|---------|---------|----------|")
+    
+    for estudiante in lista_estudiantes:
+        if estudiante["legajo"] != 0:  
+            mostrar_un_elemento(estudiante)
+            
+    print("-----------------------------------------------------------------------")
 
-
-#Opcion 4: Calcular promedio 
-def calcular_todos_los_promedios(parciales1, parciales2, promedios):
-    promedios.clear()
-    for i in range(len(parciales1)):
-        promedios_lista = (parciales1[i] + parciales2[i]) / 2.0
-        promedios.append(promedios_lista)
+# ITEM 4: Calcular promedio
+def calcular_promedio(lista_estudiantes): 
+    """Funcion que calcula el promedio de cada estudiante activo (legajo != 0)."""
+    for i in range(len(lista_estudiantes)):
+        if lista_estudiantes[i]["legajo"] != 0:
+            lista_estudiantes[i]["prom"] = (lista_estudiantes[i]["pp"] + lista_estudiantes[i]["sp"]) / 2.0
     print("Promedios calculados y guardados exitosamente.")
 
+# auxiliar 
+def obtener_promedio(estudiante):
+    """Funcion que extrae el/los promedios cargados de los estudiantes para usarlo como "llave" en la funcion de 
+    ordenamiento: sort()
+    Args: estudiante"""
+    return estudiante["prom"]
 
-#Opcion 5: Ordenar promedios DESC (descendente)
-def ordenar_por_promedio(legajos, nombres, generos, parciales1, parciales2, promedios, criterio="DESC"):
-    n = len(promedios)
-    for i in range(n - 1):
-        for j in range(n - 1 - i):
-            debe_intercambiar = False
-            # if criterio == "ASC" and promedios[j] > promedios[j + 1]:
-            #     debe_intercambiar = True
-            if criterio == "DESC" and promedios[j] < promedios[j + 1]:
-                debe_intercambiar = True
-                
-            if debe_intercambiar:
-                promedios[j], promedios[j + 1] = promedios[j + 1], promedios[j]
-                legajos[j], legajos[j + 1] = legajos[j + 1], legajos[j]
-                nombres[j], nombres[j + 1] = nombres[j + 1], nombres[j]
-                generos[j], generos[j + 1] = generos[j + 1], generos[j]
-                parciales1[j], parciales1[j + 1] = parciales1[j + 1], parciales1[j]
-                parciales2[j], parciales2[j + 1] = parciales2[j + 1], parciales2[j]
-
-
-#Opcion 6: Mostrar mejores promedios:
-def mostrar_mejores_promedios(legajos, nombres, generos, parciales1, parciales2, promedios):
-    if not promedios:
-        print("Primero debe calcular los promedios (Opción 3).")
-        return
+# ÍTEM 5: Ordenar por promedio (DESC)
+def ordenar_por_promedio(lista_estudiantes):
+    """Funcion que ordena la lista de diccionarios de forma DESC basándose en el promedio.
+    Para eso, separa los alumnos cargados de los alumnos que no han sido cargados y despues de ordenar los promedios
+    agrega a la lista los datos que correspondan
+    Args: lista_estudiantes"""
     
-    mayor_promedio = promedios[0]
-    for p in promedios:
-        if p > mayor_promedio:
-            mayor_promedio = p
+    datos_cargados = []
+    datos_no_cargados = []
+    
+   #Separa alumnos ya cargados de los que no se cargaron 
+    for estudiante in lista_estudiantes:
+        if estudiante["legajo"] != 0: 
+            datos_cargados.append(estudiante)
+        else:
+            datos_no_cargados.append(estudiante)
             
-    print(f"Estudiante(s) con el Mayor Promedio ({mayor_promedio})")
-    for i in range(len(promedios)):
-        if promedios[i] == mayor_promedio:
-            mostrar_un_elemento(legajos[i], nombres[i], generos[i], parciales1[i], parciales2[i], promedios[i])
+    datos_cargados.sort(key=obtener_promedio, reverse=True)
+    
+    lista_estudiantes.clear()
+    
+    for cargados in datos_cargados:
+        lista_estudiantes.append(cargados) 
+        
+    for no_cargados in datos_no_cargados:
+        lista_estudiantes.append(no_cargados)
 
+#ITEM 6: mostrar mejor promedio
+def mostrar_mejores_promedios(lista_estudiantes):
+    """Funcion que busca el mayor promedio entre los estudiantes cargados y los muestra.
+    Args: lista_estudiantes"""
 
-#Opcion 7: Buscar estudiante por promedio 
-def buscar_por_legajo(legajos, nombres, generos, parciales1, parciales2, promedios):
-    print("\nBúsqueda de Estudiante")
+    #Promedio de base
+    mayor_promedio = lista_estudiantes[0]["prom"]
+                        
+    for i in range(len(lista_estudiantes)):
+        if lista_estudiantes[i]["legajo"] == 0:
+            continue 
+        elif lista_estudiantes[i]["prom"] > mayor_promedio:
+            mayor_promedio = lista_estudiantes[i]["prom"]
+            
+    print(f"Estudiante(s) con el Mayor Promedio ({mayor_promedio})") 
+    print("-----------------------------------------------------------------------")
+    print("| LEGAJO | NOMBRE             | GENERO | Nota PP | Nota SP | PROMEDIO |")
+    print("|--------|--------------------|--------|---------|---------|----------|")
+    
+    for i in range(len(lista_estudiantes)):
+        if lista_estudiantes[i]["legajo"] != 0 and lista_estudiantes[i]["prom"] == mayor_promedio: 
+            mostrar_un_elemento(lista_estudiantes[i])
+    
+    print("-----------------------------------------------------------------------")
+
+# ITEM 7: Buscar por legajo
+def buscar_por_legajo(lista_estudiantes):
+    """Funcion que realiza la busqueda de un estudiante pidiendo el legajo mediante la validacion."""
+    print("Búsqueda de Estudiante")
     buscado = validar_legajo()
     encontrado = False
-    
-    for i in range(len(legajos)):
-        if legajos[i] == buscado:
-            print("\nEstudiante encontrado:")
-            if len(promedios) == 0:
-                prom = None
-            else:
-                prom = promedios[i]
-            mostrar_un_elemento(legajos[i], nombres[i], generos[i], parciales1[i], parciales2[i], prom)
+   
+    for i in range(len(lista_estudiantes)):
+        if lista_estudiantes[i]["legajo"] != 0 and lista_estudiantes[i]["legajo"] == buscado:
+           
+            print("Estudiante encontrado:")
+            print("-----------------------------------------------------------------------")
+            print("| LEGAJO | NOMBRE             | GENERO | Nota PP | Nota SP | PROMEDIO |")
+            print("|--------|--------------------|--------|---------|---------|----------|")
+
+            mostrar_un_elemento(lista_estudiantes[i])
             encontrado = True
-            break
             
+            print("-----------------------------------------------------------------------")    
+    
     if not encontrado:
-        print(f"No se encontró ningún estudiante con el legajo {buscado}.")
+        print(f"No se encontró ningún estudiante con el legajo {buscado}. ") 
+        
+#ITEM 8: Exportar a json
+def exportar_a_json(lista_estudiantes):
+    import json
+    print("Exportando a JSON...")
+    
+    alumnos_cargados = []
+
+    for estudiante in lista_estudiantes:
+        if estudiante["legajo"] != 0:
+            alumnos_cargados.append(estudiante)
+            
+    with open('prueba/data_sp.json', 'w') as archivo_json:
+        json.dump(alumnos_cargados, archivo_json, indent=4) 
+
+# ÍTEM 9: Exportar a CSV   
+def exportar_a_csv(lista_estudiantes):
+    """Exporta la lista de diccionarios a un archivo CSV sin los casilleros vacíos."""
+    print("Exportando a CSV...")
+    nombres_columnas = ["legajo", "ape_nom", "genero", "pp", "sp", "prom"]
+    
+    with open("prueba/data_sp.csv", "w") as archivo:
+        archivo.write(",".join(nombres_columnas) + "\n")
+        for estudiante in lista_estudiantes:
+            # FILTRO: Solo escribe si el alumno es real (legajo distinto de 0)
+            if estudiante['legajo'] != 0:
+                linea = f"{estudiante['legajo']},{estudiante['ape_nom']},{estudiante['genero']},{estudiante['pp']},{estudiante['sp']},{estudiante['prom']}"
+                archivo.write(linea + "\n")
+                #"\n": salto de linea
+    
+    print("Archivo 'data_sp.csv' guardado con éxito.")
+
+#Item 10: Salir del programa
+def salir_programa():
+    """Funcion que se encarga de salir del programa"""
+    print("Hasta luego!")
+    print("Saliendo del programa...")
